0e29ed4638549e5d92120c1a516887240444f5ca
Anish Lathker <alathker@uci.edu>
2023-08-05 12:50:14 -0700

    test

---
+++ app.py
@@ -0,0 +1,99 @@
+# Importing necessary libraries
+from datetime import datetime
+from dash import html, dcc, dash_table, dash
+from dash.dependencies import Input, Output
+import pandas as pd
+import trades
+
+# External stylesheets for styling the app
+external_stylesheets = [
+    {
+        'href': 'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&display=swap',
+        'rel': 'stylesheet',
+    }
+]
+
+# Initializing the Dash app with external stylesheets
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title = "TEST VER 1.0 Crypto Arbitrage Scanner"
+
+# Layout of the app
+app.layout = html.Div(
+    html.Div([
+        # Header section with title and description
+        html.Div([
+            html.H1("Arbitrage Scanner", className='header-title'),
+            html.P("Crypto Arbitrage scanner analyzing the top X tokens via coingecko and comparing them on exchanges", className='header-description'),
+        ],
+        className='header',
+        ),
+        # Table section to display trades
+        html.Div(
+            dcc.Loading(
+                dash_table.DataTable(
+                    id='trades-table',
+                    columns=[
+                        {'name': 'Coin', 'id': 'symbol'},
+                        {'name': '% Difference', 'id': 'profit'},
+                        {'name': 'High Exchange (buy)', 'id': 'highExchange'},
+                        {'name': 'Low Exchange (sell)', 'id': 'lowExchange'},
+                    ],
+                    # Styling for the table header and data
+                    style_header={
+                        'backgroundColor': 'rgb(30, 30, 30)',
+                        'color': 'white'
+                    },
+                    style_data={
+                        'backgroundColor': 'rgb(50, 50, 50)',
+                        'color': 'white'
+                    },
+                    # Enabling native sorting for the table
+                    sort_action='native'
+                ),
+            ),
+        className='table-wrapper'
+        ),
+        # Div to display the last updated time
+        html.Div(id='last-updated-text', className='last-updated'),
+        # Attribution text
+        html.P("Powered by CoinGecko", className="attribution-text"),
+        # Interval component to trigger periodic updates
+        dcc.Interval(
+            id='interval-component',
+            interval=220 * 1000,  # in milliseconds --> MUST stay above 1 minute to prevent rate limiting
+            n_intervals=0
+        )
+    ])
+)
+
+# Callback to update possible trades in the table
+@app.callback(
+    Output(component_id='trades-table', component_property='data'),
+    Input('interval-component', 'n_intervals')
+)
+def display_trades(n):
+    # Fetch data for X amount of cryptocurrencies
+    coin_data = trades.get_coin_data(15)
+    # Get possible trades based on the coin data
+    possible_trades = trades.get_trades(coin_data)
+    # Create a sorted DataFrame based on profit
+    df = trades.create_sorted_dataframe(possible_trades, 'profit')
+
+    return df.to_dict('records')
+
+# Callback to update the last updated time text
+@app.callback(
+    Output(component_id='last-updated-text', component_property='children'),
+    Input('interval-component', 'n_intervals')
+)
+def update_text(n):
+    # Get the current time and format it
+    current_time = datetime.now()
+    current_time_str = current_time.strftime("%m/%d/%Y %H:%M:%S")
+
+    return html.Span('Last updated at {ftime}.'.format(ftime=current_time_str))
+
+# Main entry point for the app
+if __name__ == "__main__":
+    app.run_server(debug=False)
---
+++ trades.py
@@ -0,0 +1,156 @@
+import json
+from time import sleep
+from pycoingecko import CoinGeckoAPI
+import pandas as pd
+import requests
+
+# Initialize CoinGecko API
+cg = CoinGeckoAPI()
+
+# Function to calculate percent difference between two numbers
+def get_change(a, b):
+    if a == b:
+        return 0
+    try:
+        return (abs(a - b) / b) * 100.0
+    except ZeroDivisionError:
+        return float('inf')
+
+# ------------------------------------------------------------------------------------------------------------
+'''
+# DEX Scanning Code (Currently only showing 0 values)
+
+# Function to calculate profit from given ticker data (cg.get_coin_ticker_by_id)
+def get_profit(coin_data):
+    tickers = coin_data['tickers']
+    prices = {}
+
+    # List of decentralized exchanges
+    dexs = ['Uniswap', 'Sushiswap', 'Balancer', 'Curve', '1inch', 'Kyber Network', '0xProtocol']
+
+    # Sort through all exchanges (with high trust scores) and add to dictionary
+    for exchange in tickers:
+        if (
+            (exchange['target'] == 'USDT' or exchange['target'] == 'USD') and
+             exchange['trust_score'] == 'green' and
+             exchange['market']['name'] in dexs  # Only include decentralized exchanges
+        ):
+            prices[str(exchange['market']['name'])] = exchange['last']
+
+    # Calculate highest and lowest from dictionary
+    high = max(prices, key=prices.get, default=0)
+    low = min(prices, key=prices.get, default=0)
+
+    # Calculate profit from potential trade
+    if (high == 0 or low == 0):
+        profit = 0
+    else:
+        profit = get_change(prices[low], prices[high])
+
+    return profit, high, low
+'''
+# ------------------------------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------------------
+# CEX Scanning Code 
+def get_profit(coin_data):
+    tickers = coin_data['tickers']
+
+    prices = {}
+
+    # sort through all exchanges (with high trust scores) and add to dictionary 
+    for exchange in tickers:
+        if (
+            (exchange['target'] == 'USDT' or exchange['target'] == 'USD') and
+             exchange['trust_score'] == 'green' and
+             exchange['market']['name'] != 'eToroX' # does not allow withdraw of crypto
+        ):
+            prices[str(exchange['market']['name'])] = exchange['last']
+    
+    # calculate hgihest and lowest from dictionary
+    high = max(prices, key=prices.get, default=0)
+    low = min(prices, key=prices.get, default=0)
+    
+    # calculate profit from potential trade
+    if (high == 0 or low == 0):
+        profit = 0
+    else:
+        profit = get_change(prices[low], prices[high])
+
+    return profit, high, low
+# ------------------------------------------------------------------------------------------------------------
+
+# Function to return an array of top coins from CoinGecko
+def get_coin_data(numCoins):
+    coin_tickers = {}
+
+    # Get list of top 100 coins by market cap
+    coin_list = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc')
+
+    # Add first 50 to dictionary (limited by CoinGecko API rate limits)
+    for coin in coin_list[:numCoins]:
+        id = coin['id']
+        sleep(12)  # Introduce a 12-second delay between requests to avoid rate limiting
+        coin_tickers[id] = cg.get_coin_ticker_by_id(id=id)
+
+    return coin_tickers
+
+# Function to get possible trades based on coin data
+def get_trades(coin_data):
+    possible_trades = {}
+
+    # Get most profitable trade for each coin
+    for ticker in coin_data:
+        profit, highExchange, lowExchange = get_profit(coin_data[ticker])
+
+        possible_trades[ticker] = {
+            'symbol': ticker,
+            'profit': profit,
+            'highExchange': highExchange,
+            'lowExchange': lowExchange
+        }
+
+    # Tether provides lots of false negatives, so remove it
+    del possible_trades['tether']
+
+    return possible_trades
+
+# Function to find the highest profit trade from possible trades
+def suggest_trade(possible_trades):
+    max_profit_ticker = ''
+    max_profit = 0
+    max_profit_highExchange = ''
+    max_profit_lowExchange = ''
+
+    # Find the most profitable trade
+    for trade in possible_trades:
+        if possible_trades[trade]['profit'] > max_profit:
+            highExchange = possible_trades[trade]['highExchange']
+            lowExchange = possible_trades[trade]['lowExchange']
+
+            max_profit = possible_trades[trade]['profit']
+            max_profit_ticker = trade
+            max_profit_highExchange = highExchange
+            max_profit_lowExchange = lowExchange
+
+    print("highest profit trade is with: " + max_profit_ticker + " for a profit of " + str(max_profit))
+    print("buy " + max_profit_ticker + " on " + max_profit_lowExchange + " and sell on " + max_profit_highExchange)
+
+# Function to create and sort a DataFrame with a key from a dictionary
+def create_sorted_dataframe(data, sort_key):
+    df = pd.DataFrame(data).T
+    df.fillna(0, inplace=True)
+    df.sort_values(by=sort_key, ascending=False, inplace=True)
+
+    return df
+
+# Test code (uncomment to run)
+'''
+coin_data = get_coin_data(50)
+possible_trades = get_trades(coin_data)
+suggest_trade(possible_trades)
+
+sorted_trades_df = create_sorted_dataframe(possible_trades, 'profit')
+
+print(sorted_trades_df)
+'''
